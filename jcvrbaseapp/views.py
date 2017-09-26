8830921a356180854be37b6c1600e9e2fcd24f4d
jucapoco <jucapocog@gmail.com>
2017-09-26 08:57:17 -0500

    New bootstrap version with lef menu toogle

--- jcvrbaseapp/views.py
+++ jcvrbaseapp/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def base_view(request):
-    return render(request, 'jcvrbaseapp/index.html', {})
+    return render(request, 'jcvrbaseapp/index3.html', {})
